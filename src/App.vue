d2281a4d6582a941fece6478b501e7afa539a063
Ahnaf Anan <aanana0171@gmail.com>
2023-08-04 02:46:07 +0600

    episode-01

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import {ref} from 'vue'
+
+const color = ref("none");
+
+const getStyle = () => {
+  return {
+    backgroundColor:color.value,
+  }
+}
+
+</script>
+
+<template>
+  <section class="mx-auto container">
+    <h1 class="text-2xl mb-10">Vue Form</h1>
+    <p class="pb-5">Your Favorite Color: {{ color }}</p>
+    <p>
+      Your Favorite Color: 
+      <input class="p-5 text-black" v-model="color" />
+      <div class="w-32 h-32 mt-10" :style="getStyle()"></div>
+    </p>
+  </section>
+</template>
+
+<style scoped>
+
+</style>
