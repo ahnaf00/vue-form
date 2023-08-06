3e32ba204a8907199f09fb7c0906f0c5008cc710
Ahnaf Anan <aanana0171@gmail.com>
2023-08-07 00:02:18 +0600

    episode-04

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,18 @@
 <script setup>
 import { ref, reactive } from 'vue'
-const displayImage = ref(true)
+const favColors = ref([]);
+
 </script>
 
 <template>
   <section class="mx-auto container">
     <h1 class="text-2xl mb-10">Vue Form</h1>
-    <p class="mb-10">{{ displayImage }}</p>
-    <label for="">Display Random Image</label>
-    <input class="ml-2" type="checkbox" v-model="displayImage"/>
-    <p class="mt-5">
-      <input type="radio" name="display" :value="true"  v-model="displayImage"> On <br/>
-      <input type="radio" name="display" :value="false" v-model="displayImage"> Off <br/>
-    </p>
-    <img
-      v-show="displayImage"
-      class="mt-10 mx-auto w-[500px]"
-      :src="`https://source.unsplash.com/random?version=${Math.random()}`"
-      alt=""  />
-      
-      <!-- <img
-      v-if="displayImage"
-      class="mt-10 mx-auto w-[500px]"
-      :src="`https://source.unsplash.com/random?version=${Math.random()}`"
-      alt=""  /> -->
-
+    <p class="mb-10">{{ favColors }}</p>
+    Your favorite colors <br/>
+    <label for="red">Red</label>
+    <input class="ml-2 mr-2" type="checkbox" id="red" v-model="favColors" value="red">
+    <label for="red">Green</label>
+    <input class="ml-2 mr-2" type="checkbox" id="red" v-model="favColors" value="green">
   </section>
 </template>
 <style scoped></style>
